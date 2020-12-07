ffb08f9eaae1446babe9785ea11efded7c94ba05
Arboghast <BlueBubbleBeam@outlook.com>
2020-12-07 14:48:10 -0500

    edge cases with disconnecting

--- index.js
+++ index.js
@@ -34,16 +34,20 @@ const User = sequelize.define('users', {
     allowNull: false,
   },
 });
+
 const LOBBY_LIMIT = 7;
 const io = require('socket.io')(8000, {cors: true});
 const redis = require('socket.io-redis');
 const Ior = require('ioredis');
-const ioredis = new Ior(6379, process.env.REDIS_HOST);
-
+const ioredis = new Ior(6379, process.env.REDIS_HOST);  //DEV
 io.adapter(redis({host: process.env.REDIS_HOST, port: 6379}));
 const randomId = require('nanoid').nanoid;
 
-//Returns the status of the lobby
+/*  LOBBYUPDATE
+  RETURN: The latest state of the lobby
+  EXAMPLE: { users: [userObj1, userObj2, userObj3...] } || {error: string}
+    where userObj = { username: string, ready: bool, leader: bool, lobbyCode: string} 
+*/
 async function updateLobby(lobbyCode) {
   const members = await io.of('/').adapter.sockets(new Set([lobbyCode])); // socket-id members
   const users = [];
@@ -58,7 +62,9 @@ async function updateLobby(lobbyCode) {
   });
 }
 
-//checks if all members of the lobby are ready
+/* Helper Function
+  RETURN: Boolean - Checks if all members of the lobby are ready
+*/
 async function readyCheck(lobbyCode) {
   const members = await io.of('/').adapter.sockets(new Set([lobbyCode]));
   let readyCheck = true;
@@ -72,7 +78,9 @@ async function readyCheck(lobbyCode) {
   return readyCheck;
 }
 
-//looks for a user within the lobby
+/*
+  RETURN: Boolean - Searches for a user within the lobby by playerName
+*/
 async function searchForUser(lobbyCode, playerName) {
   const members = await io.of('/').adapter.sockets(new Set([lobbyCode]));
   for (let it = members.values(), socketID = null; socketID = it.next().value;) { // iterate through a SET
@@ -85,53 +93,50 @@ async function searchForUser(lobbyCode, playerName) {
   return null;
 }
 
-//change user properties
-async function changeProperties(socketID, lobbyCode, ready, leader, toggle){
-  let jsonStr = await ioredis.get(socketID);
-  let json = JSON.parse(jsonStr);
-
-  if(leader != null){
-    json.leader = leader;
-  }   
-  if(ready != null){
-    json.ready = ready;
-  }
-  if(toggle){
-    json.ready = !json.ready;
-  }
-  if(json.lobbyCode != lobbyCode){
-    json.lobbyCode = lobbyCode;
-  }
-
-  jsonStr = JSON.stringify(json);
-  await ioredis.set(socketID, json);
-}
-
 io.on('connection', async (socket) => {
   console.log(`User ${socket.id} has connected`);
 
   socket.leave(socket.id); // to leave the default room
 
+  /* NEW_USER HANDLER
+    Initialize user instance in the Redis Active User Hashmap  
+  */
   socket.on('newUser', async ({username})=>{
     const json = JSON.stringify({username: username, ready: false, leader: false, lobbyCode: null});
-    await ioredis.set(socket.id, json);
+    await ioredis.set(socket.id, json)
   });
 
+  /* CREATE_LOBBY HANDLER
+    Generates a random Lobby and joins caller to the lobby.
+    Updates the caller's User Object.
+    updateLobby helper func.
+  */
   socket.on('createLobby', async ({}) =>{
     const rooms = await io.of('/').adapter.allRooms();
     let lobbyCode;
     do{
         lobbyCode = randomId(6);
     } while(rooms.has(lobbyCode));
-    //lobbyCode = 'arceux';
+    //lobbyCode = 'arceux';  //DEV
 
     socket.join(lobbyCode);
-    await changeProperties(socket.id, lobbyCode, false, true, false);
+    let str = await ioredis.get(socket.id);
+    let obj = JSON.parse(str);
+    obj.ready = false;
+    obj.leader = true;
+    obj.lobbyCode = lobbyCode;
+    str = JSON.stringify(obj);
+    await ioredis.set(socket.id, str);
+
     await updateLobby(lobbyCode);
   });
 
+  /* JOIN_LOBBY HANDLER
+    Checks for certain join conditions, if all are met then caller is joined into the Lobby.
+    Caller's user object is updated.
+    updateLobby helper func.
+  */
   socket.on('joinLobby', async ({lobbyCode}) =>{
-    //await ioredis.del(lobbyCode); // COMMENT FOR PRODUCTION
     const rooms = await io.of('/').adapter.allRooms();
     const members = await io.of('/').adapter.sockets(new Set([lobbyCode]));
     const gameInProgress = await ioredis.get(lobbyCode);
@@ -146,11 +151,25 @@ io.on('connection', async (socket) => {
       socket.emit('lobbyUpdate', {error: 'Game is in progress, please wait.'});
     } else {
       socket.join(lobbyCode);
-      await changeProperties(socket.id, lobbyCode, false, false, false);
+
+      let str = await ioredis.get(socket.id);
+      let obj = JSON.parse(str);
+      obj.ready = false;
+      obj.leader = false;
+      obj.lobbyCode = lobbyCode;
+      str = JSON.stringify(obj);
+      await ioredis.set(socket.id, str);
+
       await updateLobby(lobbyCode);
     }
   });
 
+  /* LEAVE_LOBBY HANDLER
+    Checks for leave conditions and removes caller from the Lobby.
+    Updates caller's user object.
+    Takes care of transfers of power when the caller is also the leader of the lobby.
+    updateLobby Helper func.
+  */
   socket.on('leaveLobby', async ({lobbyCode})=>{
     const members = await io.of('/').adapter.sockets(new Set([lobbyCode]));
     if (members.has(socket.id)) {
@@ -162,52 +181,107 @@ io.on('connection', async (socket) => {
         while (newLeader == null || newLeader == socket.id) {
           newLeader = ids.next().value;
         }
-        await changeProperties(newLeader, lobbyCode, null, true, false);
+
+        let str = await ioredis.get(newLeader);
+        let obj = JSON.parse(str);
+        obj.leader = true;
+        str = JSON.stringify(obj);
+        await ioredis.set(newLeader, str);
       }
       socket.leave(lobbyCode);
-      await changeProperties(socket.id, null, false, false, false);
+
+      let jsonStr = await ioredis.get(socket.id);
+      let obj = JSON.parse(jsonStr);
+      obj.ready = false;
+      obj.leader = false;
+      obj.lobbyCode = null;
+      jsonStr = JSON.stringify(obj);
+      await ioredis.set(socket.id, jsonStr);
+
       await updateLobby(lobbyCode);
     } else {
       socket.emit('lobbyUpdate', {error: 'You are not a member of this lobby'});
     }
   });
 
+  /* TOGGLE_READY HANDLER
+    Alters the caller's 'ready' property in their User Object.
+    updateLobby helper func.
+  */
   socket.on('toggleReady', async ({lobbyCode}) =>{
-    await changeProperties(socket.id, lobbyCode, null, null, true);
+    let str = await ioredis.get(socket.id);
+    let obj = JSON.parse(str);
+    obj.ready = !obj.ready;
+    str = JSON.stringify(obj);
+    await ioredis.set(socket.id, str);
     await updateLobby(lobbyCode);
   });
 
+  /* KICK_PLAYER HANDLER
+    Checks for kick conditions, if met then the unwanted player will be forcefully removed from the socket channel.
+    Sends a KICKED EVENT to the frontend for client side page navigation.
+    Alters the kicked players User Object.
+    updateLobby helper func.
+  */
   socket.on('kickPlayer', async ({lobbyCode, playerName})=>{
     const userObj = await ioredis.get(socket.id);
     const {leader} = JSON.parse(userObj);
     const userID = await searchForUser(lobbyCode, playerName);
     if (leader && userID != null) {
+      io.in(lobbyCode).emit('kicked', {playerName: playerName}); //KICKED EVENT - The player who will be removed from the lobby.
       await io.of('/').adapter.remoteLeave(userID, lobbyCode);
-      await changeProperties(userID, null, false, false, false);
+
+      let str = await ioredis.get(userID);
+      let obj = JSON.parse(str);
+      obj.ready = false;
+      obj.leader = false;
+      obj.lobbyCode = null;
+      str = JSON.stringify(obj);
+      await ioredis.set(userID, str);
+
       await updateLobby(lobbyCode);
     } else {
       socket.emit('lobbyUpdate', {error: `Unable to kick ${playerName} from the lobby`});
     }
   });
 
+  /* START_GAME HANDLER
+    readyCheck helper func, checks for start game conditons.
+    If sucessful, database call is made to retrieve a RANDOM stringified code block.
+    Updates the User Object's ready property to false for all players in the lobby.
+    Sends a RACE_INIT event containing the code block data.
+    EXAMPLE: { prompt: string } - the string is delimited via barlines | which represent new lines
+  */
   socket.on('startGame', async ({lobbyCode}) =>{
     const flag = await readyCheck(lobbyCode);
     if (flag) {
       const {prompt} = await Prompt.findOne({order: sequelize.random()});
-      await ioredis.set(lobbyCode, 0); // disallows users to join lobby while race in progress
+      await ioredis.set(lobbyCode, 0); // used to disallows users to join lobby while race in progress and count player standings
       io.to(lobbyCode).emit('raceInit', {prompt: prompt});
+      const members = await io.of('/').adapter.sockets(new Set([lobbyCode]));
       for (let it = members.values(), socketID = null; socketID = it.next().value;) { // iterate through a SET
-        await changeProperties(socketID, lobbyCode, false, null, false);
+        let str = await ioredis.get(socketID);
+        let obj = JSON.parse(str);
+        obj.ready = false;
+        str = JSON.stringify(obj);
+        await ioredis.set(socketID, str);
       }
     } else {
       io.to(lobbyCode).emit('raceInit', {error: 'Not everyone is ready to start the race.'});
     }
   });
 
+  /* LETTER_TYPED HANDLER
+    Relays typing progress from each player to the entire lobby.
+    Checks for race completion and sends a final placement representing the Users standing.
+    Deletes the lobby once all players have finished.
+    Sends the UPDATE_TEXT EVENT.
+    EXAMPLE: {playerName: string, percentage: int, placement: int}
+  */
   socket.on('letterTyped', async ({lobbyCode, percentage}) =>{ 
     const obj = await ioredis.get(socket.id);
     const {username: playerName} = JSON.parse(obj);
-    if (percentage == 100) {
+    if (percentage >= 3) {
       let pl = await ioredis.get(lobbyCode);
       io.to(lobbyCode).emit('updateText', {
         playerName: playerName,
@@ -215,7 +289,14 @@ io.on('connection', async (socket) => {
         placement: ++pl,
       });
 
-      await ioredis.set(lobbyCode, pl);
+      const members = await io.of('/').adapter.sockets(new Set([lobbyCode]));
+      if(members.size == pl){
+        await ioredis.del(lobbyCode);
+        updateLobby(lobbyCode);
+      }
+      else{
+        await ioredis.set(lobbyCode, pl);
+      }
     } else {
       io.to(lobbyCode).emit('updateText', {
         playerName: playerName,
@@ -224,24 +305,40 @@ io.on('connection', async (socket) => {
     }
   });
 
+  /* DISCONNECTING HANDLER - CALLED AUTOMATICALLY
+    Similar logic to the leaveLobby handler - except we dont have access to any data via parameters.
+    If a temporary User Object is found, we will delete it from both Redis and our SQL database.
+  */
   socket.on('disconnecting', async (reason) =>{
     const user = await ioredis.get(socket.id);
     if (user != null) {
-      const {lobby: lobbyCode, leader, username} = JSON.parse(user);
+      const {lobbyCode, leader, username} = JSON.parse(user);
       if(lobbyCode != null){
           const members = await io.of('/').adapter.sockets(new Set([lobbyCode]));
         if (leader && members.size > 1) {
           const ids = members.values();
-          let newLeader;
-          do{
+          let newLeader = null;
+          while (newLeader == null || newLeader == socket.id) {
             newLeader = ids.next().value;
-          } while (newLeader == socket.id);
-          await changeProperties(newLeader, lobbyCode, null, true, false);
+          }
+
+          let str = await ioredis.get(newLeader);
+          let obj = JSON.parse(str);
+          obj.leader = true;
+          str = JSON.stringify(obj);
+          await ioredis.set(newLeader, str);
         }
 
         const status = await ioredis.get(lobbyCode);
         if (status != null) { // implies that game is in progress
-          socket.to(lobbyCode).emit('updateText', {error: `User ${username} has disconnected`} );
+          let pl = await ioredis.get(lobbyCode);
+          const members = await io.of('/').adapter.sockets(new Set([lobbyCode]));
+          if(members.size == pl){
+            await ioredis.del(lobbyCode);
+            await updateLobby(lobbyCode);
+          }else{
+            socket.to(lobbyCode).emit('updateText', {error: `User ${username} has disconnected`} );
+          }
         } else {
           await updateLobby(lobbyCode);
         } 
